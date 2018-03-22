f5fecbb2d7a66cb1ab37bfdc457b002ea1708d39
MrHZJ <web_zhijun@163.com>
2018-03-22 17:22:56 +0800

    完善版

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,7 @@
 	var coms = window.document.querySelectorAll(".comment");
 	
 	for (var i =  coms.length - 1; i >= 0; i--) {
-		var item = coms[i];
-
+		var item = coms[i] ;
 		item.innerHTML = '+ ' + item.innerHTML;
 	}
 
