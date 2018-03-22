0401c9196628a94bd4b1de5957d295606bbbf6b2
MrHZJ <web_zhijun@163.com>
2018-03-22 18:02:39 +0800

    完善版

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+(function(window){
+	var coms = window.document.querySelectorAll(".comment");
+	
+	for (var i =  coms.length - 1; i >= 0; i--) {
+		var item = coms[i] ;
+		item.innerHTML = '+ ' + item.innerHTML;
+	}
+
+	var subcoms = window.document.querySelectorAll(".subcom");
+	
+	for (var i =  subcoms.length - 1; i >= 0; i--) {
+		var item = subcoms[i];
+		
+		item.innerHTML = '- ' + item.innerHTML;
+	}
+})(window)
